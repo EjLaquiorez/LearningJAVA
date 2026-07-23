00528a89506f337bb32ffd366385274ccef53726
EjLaquiorez <ejlqrz@gmail.com>
2026-07-23 17:25:41 +0800

    hands on exercises

---
+++ exercises/dreamJob.java
@@ -0,0 +1,22 @@
+public class dreamJob {
+    public static void main(String[] args) {
+        
+        String title = "===== DREAM JOB =====";
+        String company = "Google";
+        String position = "Java Backend Engineer";
+        int expectedSalary = 120000;
+        String experienceRequired = "2 years";
+        String country = "Singapore";
+
+        System.out.println(title);
+        System.out.println("Company: " + company);
+        System.out.println("Position: " + position);
+        System.out.println("Expected Salary: " + expectedSalary);
+        System.out.println("Experience Required: " + experienceRequired);
+        System.out.println("Country: " + country);
+
+    }
+}
+
+
+    
---
+++ exercises/studentProfile.java
@@ -0,0 +1,26 @@
+public class studentProfile {
+   public static void main(String[] args) {
+
+        String name = "Justine";
+        int age = 20;
+        double height = 5.6;
+        float weight = 60.5f;
+        float GPA = 1.5f;
+        char firstInitial = 'J';
+        boolean enrolled = true;
+
+
+
+
+        System.out.println("================================");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("GPA: " + GPA);
+        System.out.println("First Initial: " + firstInitial);
+        System.out.println("Enrolled: " + enrolled);
+        System.out.println("================================");
+    }
+    
+}
