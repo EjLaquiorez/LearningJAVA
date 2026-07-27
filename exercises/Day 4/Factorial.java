c4951636388fd0654834b25eea8055dd9833c595
EjLaquiorez <ejlqrz@gmail.com>
2026-07-27 11:44:08 +0800

    refactor coding exercises

--- exercises/Day 4/Factorial.java
+++ exercises/Day 4/Factorial.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class Factorial {
-    public static void main(String[] args){
-        int number = 1;
-        for (int i = 5; i >= 1; i--) {
-            number *= i;
-                if(i > 1){
-                    System.out.print(i + "x" );
-                }
-                else{
-                    System.out.print(i);
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.print("Enter a number: ");
+            int n = scanner.nextInt();
+
+            // Guard Clause
+            if (n < 0) {
+                System.out.println("Factorial is not defined for negative numbers.");
+                return;
             }
-            
+
+            int factorial = 1;
+
+            for (int i = n; i >= 1; i--) {
+                factorial *= i;
+
+                if (i > 1) {
+                    System.out.print(i + " x ");
+                } else {
+                    System.out.print(i);
+                }
+            }
+
+            System.out.println(" = " + factorial);
         }
-        System.out.println("=" + number);
     }
 }
